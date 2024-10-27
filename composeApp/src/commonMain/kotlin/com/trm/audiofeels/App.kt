f1c1638f1aefeccdf51e1b9cd6b1a917d9a3bbcf
merengue <therealmerengue@proton.me>
2024-10-27 11:53:41 +0100

    Remove useless files

--- composeApp/src/commonMain/kotlin/com/trm/audiofeels/App.kt
+++ composeApp/src/commonMain/kotlin/com/trm/audiofeels/App.kt
@@ -23,11 +23,7 @@ fun App() {
     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
       Button(onClick = { showContent = !showContent }) { Text("Click me!") }
       AnimatedVisibility(showContent) {
-        val greeting = remember { Greeting().greet() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-          Image(painterResource(Res.drawable.compose_multiplatform), null)
-          Text("Compose: $greeting")
-        }
+        Image(painterResource(Res.drawable.compose_multiplatform), null)
       }
     }
   }
--- composeApp/src/commonMain/kotlin/com/trm/audiofeels/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.trm.audiofeels
-
-interface Platform {
-  val name: String
-}
-
-expect fun getPlatform(): Platform
