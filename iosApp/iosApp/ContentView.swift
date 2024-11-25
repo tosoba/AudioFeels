0b490023246dc89bd6052ff9bc8ca147a61e9b6b
merengue <therealmererengue@proton.me>
2024-11-25 21:13:39 +0100

    Add IosApplicationComponent

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,7 +2,7 @@ import UIKit
 import SwiftUI
 import ComposeApp
 
-struct ComposeView: UIViewControllerRepresentable {
+private struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
         MainViewControllerKt.MainViewController()
     }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+    private let component = IosApplicationComponent.companion.create()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
