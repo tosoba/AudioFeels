f287fe93046a8988f2f0b5174d10c85a2d7ffac4
merengue <therealmererengue@proton.me>
2024-11-30 13:29:18 +0100

    Add calculateDisplayType implementation

--- core/ui/compose/build.gradle.kts
+++ core/ui/compose/build.gradle.kts
@@ -19,6 +19,8 @@ kotlin {
         implementation(compose.material)
         implementation(compose.materialIconsExtended)
         api(compose.material3)
+
+        implementation(libs.material3.windowSizeClass)
       }
     }
   }
--- core/ui/compose/src/commonMain/kotlin/com/trm/audiofeels/core/ui/compose/util/DisplayType.kt
+++ core/ui/compose/src/commonMain/kotlin/com/trm/audiofeels/core/ui/compose/util/DisplayType.kt
@@ -1,6 +1,33 @@
 package com.trm.audiofeels.core.ui.compose.util
 
-enum class AppContentType {
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+
+enum class DisplayType {
   SINGLE_PANE,
   DUAL_PANE,
 }
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun calculateDisplayType(displayFeaturesManager: DisplayFeaturesManager): DisplayType =
+  when (calculateWindowSizeClass().widthSizeClass) {
+    WindowWidthSizeClass.Compact -> {
+      DisplayType.SINGLE_PANE
+    }
+    WindowWidthSizeClass.Medium -> {
+      if (!displayFeaturesManager.isNormalDevicePosture()) {
+        DisplayType.DUAL_PANE
+      } else {
+        DisplayType.SINGLE_PANE
+      }
+    }
+    WindowWidthSizeClass.Expanded -> {
+      DisplayType.DUAL_PANE
+    }
+    else -> {
+      DisplayType.SINGLE_PANE
+    }
+  }
