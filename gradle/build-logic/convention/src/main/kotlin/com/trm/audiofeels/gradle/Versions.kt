ed061fdea471e590cd880638317573462c2bc02c
tosoba <therealmerengue@gmail.com>
2024-12-25 11:00:06 +0100

    Upgrade compile/target SDK to 35

--- gradle/build-logic/convention/src/main/kotlin/com/trm/audiofeels/gradle/Versions.kt
+++ gradle/build-logic/convention/src/main/kotlin/com/trm/audiofeels/gradle/Versions.kt
@@ -1,7 +1,7 @@
 package com.trm.audiofeels.gradle
 
 object Versions {
-  const val COMPILE_SDK = 34
+  const val COMPILE_SDK = 35
   const val MIN_SDK = 24
-  const val TARGET_SDK = 34
+  const val TARGET_SDK = 35
 }
