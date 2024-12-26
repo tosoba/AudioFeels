c5d998f2ed53a31157f154343998ad1268747bbb
tosoba <therealmerengue@gmail.com>
2024-12-26 14:44:48 +0100

    Move mediaLibrarySession creation to PlayerServiceComponent

--- player/src/androidMain/kotlin/com/trm/audiofeels/core/player/PlayerService.kt
+++ player/src/androidMain/kotlin/com/trm/audiofeels/core/player/PlayerService.kt
@@ -1,51 +1,20 @@
 package com.trm.audiofeels.core.player
 
-import androidx.media3.common.AudioAttributes
-import androidx.media3.common.C
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.ExoPlayer
 import com.trm.audiofeels.core.base.LifecycleMediaLibraryService
 import com.trm.audiofeels.core.player.di.PlayerServiceComponent
 import com.trm.audiofeels.core.player.di.PlayerServiceComponentFactory
 
 @UnstableApi
 class PlayerService : LifecycleMediaLibraryService() {
-  override var mediaLibrarySession: MediaLibrarySession? = null
-
   private val component: PlayerServiceComponent by
     lazy(LazyThreadSafetyMode.NONE) { (application as PlayerServiceComponentFactory).create(this) }
 
+  override val mediaLibrarySession: MediaLibrarySession by
+    lazy(LazyThreadSafetyMode.NONE) { component.mediaLibrarySession }
+
   override fun onCreate() {
     super.onCreate()
-    mediaLibrarySession = buildMediaLibrarySession(buildExoPlayer(buildAudioAttributes()))
     setMediaNotificationProvider(component.playerNotificationProvider)
-  }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    releaseSession()
-  }
-
-  private fun buildMediaLibrarySession(player: ExoPlayer) =
-    MediaLibrarySession.Builder(this, player, component.playerSessionCallback).build()
-
-  private fun buildAudioAttributes(): AudioAttributes =
-    AudioAttributes.Builder()
-      .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
-      .setUsage(C.USAGE_MEDIA)
-      .build()
-
-  private fun buildExoPlayer(audioAttributes: AudioAttributes): ExoPlayer =
-    ExoPlayer.Builder(this)
-      .setAudioAttributes(audioAttributes, true)
-      .setHandleAudioBecomingNoisy(true)
-      .build()
-
-  private fun releaseSession() {
-    mediaLibrarySession?.run {
-      player.release()
-      release()
-      mediaLibrarySession = null
-    }
   }
 }
--- player/src/androidMain/kotlin/com/trm/audiofeels/core/player/di/PlayerServiceComponent.kt
+++ player/src/androidMain/kotlin/com/trm/audiofeels/core/player/di/PlayerServiceComponent.kt
@@ -1,9 +1,17 @@
 package com.trm.audiofeels.core.player.di
 
 import android.app.Service
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaLibraryService
+import androidx.media3.session.MediaLibraryService.MediaLibrarySession
 import com.trm.audiofeels.core.player.PlayerNotificationProvider
 import com.trm.audiofeels.core.player.PlayerSessionCallback
+import me.tatarka.inject.annotations.Provides
 
 interface PlayerServiceComponent {
   val service: Service
@@ -11,4 +19,35 @@ interface PlayerServiceComponent {
   @get:UnstableApi val playerNotificationProvider: PlayerNotificationProvider
 
   val playerSessionCallback: PlayerSessionCallback
+
+  val mediaLibrarySession: MediaLibrarySession
+
+  @Provides
+  fun mediaLibrarySession(): MediaLibrarySession =
+    MediaLibrarySession.Builder(
+        service as MediaLibraryService,
+        ExoPlayer.Builder(service)
+          .setAudioAttributes(
+            AudioAttributes.Builder()
+              .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC)
+              .setUsage(C.USAGE_MEDIA)
+              .build(),
+            true,
+          )
+          .setHandleAudioBecomingNoisy(true)
+          .build(),
+        playerSessionCallback,
+      )
+      .build()
+      .also {
+        (service as LifecycleOwner)
+          .lifecycle
+          .addObserver(
+            object : DefaultLifecycleObserver {
+              override fun onDestroy(owner: LifecycleOwner) {
+                it.release()
+              }
+            }
+          )
+      }
 }
