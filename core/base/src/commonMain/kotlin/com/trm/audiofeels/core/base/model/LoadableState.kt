6a671d8ed81ab279f901fa8226178fd28124b0d9
merengue <therealmererengue@proton.me>
2025-01-02 19:40:44 +0100

    Add timeout to loadableStateFlowOf + remove Interactor.kt

--- core/base/src/commonMain/kotlin/com/trm/audiofeels/core/base/model/LoadableState.kt
+++ core/base/src/commonMain/kotlin/com/trm/audiofeels/core/base/model/LoadableState.kt
@@ -1,9 +1,12 @@
 package com.trm.audiofeels.core.base.model
 
 import co.touchlab.kermit.Logger
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withTimeout
 
 sealed interface LoadableState<out T> {
   data object Loading : LoadableState<Nothing>
@@ -13,12 +16,17 @@ sealed interface LoadableState<out T> {
   data class Error(val throwable: Throwable?) : LoadableState<Nothing>
 }
 
-fun <T> loadableStateFlowOf(load: suspend () -> T): Flow<LoadableState<T>> =
+fun <T> loadableStateFlowOf(
+  timeout: Duration = DefaultTimeout,
+  load: suspend () -> T,
+): Flow<LoadableState<T>> =
   flow {
       emit(LoadableState.Loading)
-      emit(LoadableState.Success(load()))
+      emit(LoadableState.Success(withTimeout(timeout) { load() }))
     }
     .catch {
       Logger.e(messageString = "LoadableStateError", throwable = it)
       emit(LoadableState.Error(it))
     }
+
+internal val DefaultTimeout = 20.seconds
