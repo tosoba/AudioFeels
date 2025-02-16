84f6286ddf11f2e43b3ce3d59933d6394ee7ea67
merengue <therealmererengue@proton.me>
2025-02-16 14:48:23 +0100

    Replace carousel with HorizontalPager

--- ui/player/src/commonMain/kotlin/com/trm/audiofeels/ui/player/composable/PlayerExpandedContent.kt
+++ ui/player/src/commonMain/kotlin/com/trm/audiofeels/ui/player/composable/PlayerExpandedContent.kt
@@ -1,70 +1,68 @@
 package com.trm.audiofeels.ui.player.composable
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
-import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
 import com.trm.audiofeels.core.ui.compose.AsyncShimmerImage
 import com.trm.audiofeels.core.ui.compose.util.shimmerBackground
 import com.trm.audiofeels.ui.player.PlayerViewState
+import kotlin.math.absoluteValue
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayerExpandedContent(viewState: PlayerViewState, modifier: Modifier = Modifier) {
-  Column(modifier = modifier) {
-    BoxWithConstraints(
-      modifier =
-        Modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 12.dp)
-    ) {
+  Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    Crossfade(viewState) {
       when (viewState) {
         is PlayerViewState.Invisible,
-        is PlayerViewState.Error,
+        is PlayerViewState.Error, // TODO: retry
         is PlayerViewState.Loading -> {
           CircularProgressIndicator()
         }
         is PlayerViewState.Playback -> {
-          val carouselState =
-            rememberCarouselState(viewState.currentTrackIndex) { viewState.tracks.size }
-          HorizontalMultiBrowseCarousel(
-            state = carouselState,
-            preferredItemWidth = maxWidth / 2,
-            contentPadding = PaddingValues(8.dp),
-          ) {
-            AsyncShimmerImage(
-              model = viewState.tracks.getOrNull(it)?.artworkUrl,
-              contentDescription = null,
-              modifier = { enabled ->
-                Modifier.maskClip(MaterialTheme.shapes.extraLarge)
-                  .shimmerBackground(enabled = enabled, shape = MaterialTheme.shapes.extraLarge)
-              },
+          val pagerState =
+            rememberPagerState(
+              initialPage = viewState.currentTrackIndex,
+              pageCount = viewState.tracks::size,
             )
+          BoxWithConstraints(modifier = Modifier.weight(1f)) {
+            HorizontalPager(
+              state = pagerState,
+              contentPadding = PaddingValues(horizontal = 64.dp, vertical = 16.dp),
+              modifier = Modifier.fillMaxWidth(),
+            ) {
+              val pageOffset = (pagerState.currentPage - it) + pagerState.currentPageOffsetFraction
+              AsyncShimmerImage(
+                model = viewState.tracks.getOrNull(it)?.artworkUrl,
+                contentDescription = null,
+                modifier = { enabled ->
+                  Modifier.aspectRatio(1f)
+                    .graphicsLayer {
+                      val fraction = 1f - pageOffset.absoluteValue.coerceIn(0f, 1f)
+                      val scale = lerp(start = 0.85.dp, stop = 1.dp, fraction = fraction)
+                      scaleX = scale.value
+                      scaleY = scale.value
+                      alpha = lerp(start = 0.5.dp, stop = 1.dp, fraction = fraction).value
+                    }
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .shimmerBackground(enabled = enabled, shape = MaterialTheme.shapes.extraLarge)
+                },
+              )
+            }
           }
-        }
-      }
-    }
-
-    LazyColumn(
-      contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 12.dp),
-      modifier = Modifier.fillMaxWidth().weight(1f),
-    ) {
-      when (viewState) {
-        is PlayerViewState.Invisible,
-        is PlayerViewState.Error,
-        is PlayerViewState.Loading -> {}
-        is PlayerViewState.Playback -> {
-          items(viewState.tracks) { Text(it.title) }
         }
       }
     }
