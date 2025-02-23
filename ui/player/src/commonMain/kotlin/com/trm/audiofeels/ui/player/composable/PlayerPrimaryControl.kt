159da6cec0a08d06aec36bcd6f8b149e3de71e6c
merengue <therealmererengue@proton.me>
2025-02-23 14:35:30 +0100

    Put CircularProgressIndicator inside PlayerPrimaryControl

--- ui/player/src/commonMain/kotlin/com/trm/audiofeels/ui/player/composable/PlayerPrimaryControl.kt
+++ ui/player/src/commonMain/kotlin/com/trm/audiofeels/ui/player/composable/PlayerPrimaryControl.kt
@@ -1,5 +1,6 @@
 package com.trm.audiofeels.ui.player.composable
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.Crossfade
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilledIconButton
@@ -9,14 +10,17 @@ import com.trm.audiofeels.ui.player.PlayerViewState
 
 @Composable
 internal fun PlayerPrimaryControl(controlState: PlayerViewState.PrimaryControlState) {
-  Crossfade(controlState) {
-    when (it) {
-      PlayerViewState.PrimaryControlState.Loading -> {
-        CircularProgressIndicator()
-      }
-      is PlayerViewState.PrimaryControlState.Action -> {
-        FilledIconButton(onClick = it.action) {
+  FilledIconButton(
+    onClick = { (controlState as? PlayerViewState.PrimaryControlState.Action)?.action?.invoke() },
+    enabled = controlState is PlayerViewState.PrimaryControlState.Action,
+  ) {
+    AnimatedContent(controlState) {
+      when (it) {
+        is PlayerViewState.PrimaryControlState.Action -> {
           Icon(imageVector = it.imageVector, contentDescription = it.contentDescription)
+        }
+        PlayerViewState.PrimaryControlState.Loading -> {
+          CircularProgressIndicator()
         }
       }
     }
