28982c3387ae595a7c7d73f49799865050175858
merengue <therealmererengue@proton.me>
2025-03-05 20:15:46 +0100

    Add SgFilter converted to kotlin

---
+++ player/src/androidMain/kotlin/com/trm/audiofeels/core/player/visualizer/filter/SgFilter.kt
@@ -0,0 +1,72 @@
+package com.trm.audiofeels.core.player.visualizer.filter
+
+import com.trm.audiofeels.core.player.visualizer.filter.SgFilters.computeWeights
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+class SgFilter(
+  private val windowSize: Int,
+  private val derivative: Int = 0,
+  private val polynomial: Int = 3,
+) {
+  private val weights: Array<FloatArray>
+
+  init {
+    require(!(windowSize % 2 == 0 || windowSize < 5)) {
+      "options.WindowSize [$windowSize] must be odd and equal to or greater than 5"
+    }
+    require(derivative >= 0) { "options.Derivative [$derivative] must be equal or greater than 0" }
+    require(polynomial >= 0) { "options.Polynomial [$polynomial] must be equal or greater than 0" }
+    this.weights = computeWeights(windowSize, polynomial, derivative)
+  }
+
+  fun getHs(h: FloatArray, center: Int, half: Int, derivative: Int): Float {
+    var hs = 0f
+    var count = 0
+    for (i in center - half until center + half) {
+      if (i >= 0 && i < h.size - 1) {
+        hs += h[i + 1] - h[i]
+        count++
+      }
+    }
+    return (hs / count).pow(derivative)
+  }
+
+  fun process(data: FloatArray, h: FloatArray, out: FloatArray) {
+    require(windowSize <= data.size) {
+      "data length [${data.size}] must be larger than options.WindowSize[$windowSize]"
+    }
+
+    require(data.size == out.size) { "data/output size mismatch [${data.size}, ${out.size}]" }
+
+    val halfWindow = floor(windowSize / 2.0).roundToInt()
+    val numPoints = data.size
+    var hs: Float
+
+    for (i in 0 until halfWindow) {
+      val wg1 = weights[halfWindow - i - 1]
+      val wg2 = weights[halfWindow + i + 1]
+      var d1 = 0f
+      var d2 = 0f
+      for (l in 0 until windowSize) {
+        d1 += wg1[l] * data[l]
+        d2 += wg2[l] * data[numPoints - windowSize + l]
+      }
+      hs = getHs(h, halfWindow - i - 1, halfWindow, derivative)
+      out[halfWindow - i - 1] = d1 / hs
+      hs = getHs(h, numPoints - halfWindow + i, halfWindow, derivative)
+      out[numPoints - halfWindow + i] = d2 / hs
+    }
+
+    val wg = weights[halfWindow]
+    for (i in windowSize..numPoints) {
+      var d = 0f
+      for (l in 0 until windowSize) {
+        d += wg[l] * data[l + i - windowSize]
+      }
+      hs = getHs(h, i - halfWindow - 1, halfWindow, derivative)
+      out[i - halfWindow - 1] = d / hs
+    }
+  }
+}
---
+++ player/src/androidMain/kotlin/com/trm/audiofeels/core/player/visualizer/filter/SgFilters.kt
@@ -0,0 +1,68 @@
+package com.trm.audiofeels.core.player.visualizer.filter
+
+import kotlin.math.floor
+import kotlin.math.roundToInt
+
+internal object SgFilters {
+  fun gramPolynomial(i: Int, m: Int, k: Int, s: Int): Float =
+    if (k > 0) {
+      val t0 = (4 * k - 2).toFloat()
+      val t1 = (k * (2 * m - k + 1)).toFloat()
+      val t2 = t0 / t1
+
+      val t3 = i * gramPolynomial(i, m, k - 1, s)
+      val t4 = s * gramPolynomial(i, m, k - 1, s - 1)
+
+      val t5 = ((k - 1) * (2 * m + k)).toFloat()
+      val t6 = (k * (2 * m - k + 1)).toFloat()
+      val t7 = t5 / t6
+
+      val t8 = gramPolynomial(i, m, k - 2, s)
+
+      t2 * (t3 + t4) - t7 * t8
+    } else {
+      if (k == 0 && s == 0) {
+        1f
+      } else {
+        0f
+      }
+    }
+
+  fun productOfRange(a: Int, b: Int): Int {
+    var gf = 1
+    if (a >= b) {
+      for (j in a - b + 1..a) {
+        gf *= j
+      }
+    }
+    return gf
+  }
+
+  fun polyWeight(i: Int, t: Int, windowMiddle: Int, polynomial: Int, derivative: Int): Float {
+    var sum = 0f
+    for (k in 0..polynomial) {
+      val t0 = (2 * k + 1).toFloat()
+      val t1 = productOfRange(2 * windowMiddle, k).toFloat()
+      val t2 = productOfRange(2 * windowMiddle + k + 1, k + 1).toFloat()
+      val t3 = gramPolynomial(i, windowMiddle, k, 0)
+      val t4 = gramPolynomial(t, windowMiddle, k, derivative)
+      sum += t0 * (t1 / t2) * t3 * t4
+    }
+    return sum
+  }
+
+  fun computeWeights(windowSize: Int, polynomial: Int, derivative: Int): Array<FloatArray> {
+    val weights = Array(windowSize) { floatArrayOf() }
+    val windowMiddle = floor(windowSize / 2.0).roundToInt()
+
+    for (row in -windowMiddle..windowMiddle) {
+      weights[row + windowMiddle] = FloatArray(windowSize)
+      for (col in -windowMiddle..windowMiddle) {
+        weights[row + windowMiddle][col + windowMiddle] =
+          polyWeight(col, row, windowMiddle, polynomial, derivative)
+      }
+    }
+
+    return weights
+  }
+}
