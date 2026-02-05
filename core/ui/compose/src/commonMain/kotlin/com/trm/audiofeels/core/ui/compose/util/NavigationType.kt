3c24d182250ba6ab6250abaed97256905862131e
tosoba <therealmerengue@gmail.com>
2026-02-05 20:50:05 +0100

    Replace deprecated width size class usage

--- core/ui/compose/src/commonMain/kotlin/com/trm/audiofeels/core/ui/compose/util/NavigationType.kt
+++ core/ui/compose/src/commonMain/kotlin/com/trm/audiofeels/core/ui/compose/util/NavigationType.kt
@@ -2,9 +2,10 @@ package com.trm.audiofeels.core.ui.compose.util
 
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.window.core.layout.WindowWidthSizeClass
 
 enum class NavigationType(val suiteType: NavigationSuiteType) {
   NAVIGATION_BAR(NavigationSuiteType.NavigationBar),
@@ -12,13 +13,14 @@ enum class NavigationType(val suiteType: NavigationSuiteType) {
   PERMANENT_NAVIGATION_DRAWER(NavigationSuiteType.NavigationDrawer);
 
   companion object {
+    @Composable
     operator fun invoke(adaptiveInfo: WindowAdaptiveInfo, windowSize: DpSize): NavigationType =
       when {
         adaptiveInfo.windowPosture.isTabletop ||
-          adaptiveInfo.windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.COMPACT -> {
+          currentWindowWidthClass() == WindowWidthSizeClass.Compact -> {
           NAVIGATION_BAR
         }
-        adaptiveInfo.windowSizeClass.windowWidthSizeClass == WindowWidthSizeClass.EXPANDED &&
+        currentWindowWidthClass() == WindowWidthSizeClass.Expanded &&
           windowSize.width >= 1200.dp -> {
           PERMANENT_NAVIGATION_DRAWER
         }
