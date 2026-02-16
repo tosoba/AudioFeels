bfaa1088d18114120a2013a2e049b037ae08503d
tosoba <therealmerengue@gmail.com>
2026-02-16 19:18:22 +0100

    Add ic_launcher to DiscoverTopBar

--- ui/discover/src/commonMain/kotlin/com/trm/audiofeels/ui/discover/DiscoverTopBar.kt
+++ ui/discover/src/commonMain/kotlin/com/trm/audiofeels/ui/discover/DiscoverTopBar.kt
@@ -1,5 +1,8 @@
 package com.trm.audiofeels.ui.discover
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -7,12 +10,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.trm.audiofeels.core.ui.compose.theme.Spacing
 import com.trm.audiofeels.core.ui.compose.theme.topAppBarColorsWithGradient
 import com.trm.audiofeels.core.ui.compose.util.defaultHazeEffect
 import com.trm.audiofeels.core.ui.resources.Res
 import com.trm.audiofeels.core.ui.resources.app_name
+import com.trm.audiofeels.core.ui.resources.ic_launcher
 import dev.chrisbanes.haze.HazeState
 import dev.chrisbanes.haze.HazeStyle
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,6 +30,13 @@ internal fun DiscoverTopBar(hazeState: HazeState) {
       Text(
         text = stringResource(Res.string.app_name),
         style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+      )
+    },
+    navigationIcon = {
+      Image(
+        painter = painterResource(Res.drawable.ic_launcher),
+        contentDescription = null,
+        modifier = Modifier.size(48.dp).padding(Spacing.small8dp),
       )
     },
     colors = topAppBarColorsWithGradient(),
